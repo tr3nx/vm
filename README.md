dafc79c328fc4c2eac8a2aeccbff1c073539dee9
tr3nx <0wnedbytr3nx@gmail.com>
2020-04-11 20:47:23 -0700

    Stack Machine using Nim lang

--- README.md
+++ README.md
@@ -1,1 +1,171 @@
-### Stack vm in Nim
+# Stack Machine using Nim lang
+
+
+```
+proc fact(n: int): int =
+  return if n < 2: 1
+             else: n * fact(n - 1)
+```
+
+```
+# if n < 2: 1 else:
+Arg       # 0
+0         # 1
+Push      # 2
+2         # 3
+Lt        # 4
+JumpFalse # 5
+10        # 6
+Push      # 7
+1         # 8
+Ret       # 9
+
+# n * fact(n - 1)
+Arg       # 10
+0         # 11
+Arg       # 12
+0         # 13
+Push      # 14
+1         # 15
+Sub       # 16
+Call      # 17
+0         # 18
+1         # 19
+Mul       # 20
+Ret       # 21
+
+# proc fact(n: int): int
+Push      # 22 - entry
+2         # 23 - input value
+Call      # 24
+0         # 25
+1         # 26
+```
+
+```
+[tr3nx@box hvm]$ nim c -r vm.nim 
+Hint: used config file '/etc/nim/nim.cfg' [Conf]
+Hint: system [Processing]
+Hint: widestrs [Processing]
+Hint: io [Processing]
+Hint: vm [Processing]
+CC: vm.nim
+Hint:  [Link]
+Hint: operation successful (14708 lines compiled; 0.255 sec total; 20.742MiB peakmem; Debug Build) [SuccessX]
+Hint: /home/tr3nx/coding/nim/hvm/vm  [Exec]
+---------------------
+ ip = 24 | fp = 0
+    Code | Push (9)
+   Stack | @[2]
+  Memory | []
+
+---------------------
+ ip = 0 | fp = 3
+    Code | Call (17)
+   Stack | @[2, 1, 0, 27]
+  Memory | []
+
+---------------------
+ ip = 2 | fp = 3
+    Code | Arg (18)
+   Stack | @[2, 1, 0, 27, 2]
+  Memory | []
+
+---------------------
+ ip = 4 | fp = 3
+    Code | Push (9)
+   Stack | @[2, 1, 0, 27, 2, 2]
+  Memory | []
+
+---------------------
+ ip = 5 | fp = 3
+    Code | Lt (7)
+   Stack | @[2, 1, 0, 27, 0]
+  Memory | []
+
+---------------------
+ ip = 10 | fp = 3
+    Code | JumpFalse (16)
+   Stack | @[2, 1, 0, 27]
+  Memory | []
+
+---------------------
+ ip = 12 | fp = 3
+    Code | Arg (18)
+   Stack | @[2, 1, 0, 27, 2]
+  Memory | []
+
+---------------------
+ ip = 14 | fp = 3
+    Code | Arg (18)
+   Stack | @[2, 1, 0, 27, 2, 2]
+  Memory | []
+
+---------------------
+ ip = 16 | fp = 3
+    Code | Push (9)
+   Stack | @[2, 1, 0, 27, 2, 2, 1]
+  Memory | []
+
+---------------------
+ ip = 17 | fp = 3
+    Code | Sub (2)
+   Stack | @[2, 1, 0, 27, 2, 1]
+  Memory | []
+
+---------------------
+ ip = 0 | fp = 8
+    Code | Call (17)
+   Stack | @[2, 1, 0, 27, 2, 1, 1, 3, 20]
+  Memory | []
+
+---------------------
+ ip = 2 | fp = 8
+    Code | Arg (18)
+   Stack | @[2, 1, 0, 27, 2, 1, 1, 3, 20, 1]
+  Memory | []
+
+---------------------
+ ip = 4 | fp = 8
+    Code | Push (9)
+   Stack | @[2, 1, 0, 27, 2, 1, 1, 3, 20, 1, 2]
+  Memory | []
+
+---------------------
+ ip = 5 | fp = 8
+    Code | Lt (7)
+   Stack | @[2, 1, 0, 27, 2, 1, 1, 3, 20, 1]
+  Memory | []
+
+---------------------
+ ip = 7 | fp = 8
+    Code | JumpFalse (16)
+   Stack | @[2, 1, 0, 27, 2, 1, 1, 3, 20]
+  Memory | []
+
+---------------------
+ ip = 9 | fp = 8
+    Code | Push (9)
+   Stack | @[2, 1, 0, 27, 2, 1, 1, 3, 20, 1]
+  Memory | []
+
+---------------------
+ ip = 20 | fp = 3
+    Code | Ret (19)
+   Stack | @[2, 1, 0, 27, 2, 1]
+  Memory | []
+
+---------------------
+ ip = 21 | fp = 3
+    Code | Mul (3)
+   Stack | @[2, 1, 0, 27, 2]
+  Memory | []
+
+---------------------
+ ip = 27 | fp = 0
+    Code | Ret (19)
+   Stack | @[2]
+  Memory | []
+
+2
+```
--- vm.nim
+++ vm.nim
@@ -1,146 +1,225 @@
-# HVM
+# Stack Machine using Nim lang
 
 type
   OpCode = enum
-    False,
-    True,
-    Add,
-    Sub,
-    Mul,
-    Div,
-    Gt,
-    Lt,
-    Eq,
-    Push,
-    Pop,
-    Store,
-    Fetch,
-    Jump,
-    JumpFalse,
-    JumpTrue,
+    False,     # 0
+    True,      # 1
+    Add,       # 2
+    Sub,       # 3
+    Mul,       # 4
+    Div,       # 5
+    Mod,       # 6
+    Gt,        # 7
+    Lt,        # 8
+    Eq,        # 9
+    Push,      # 10
+    Pop,       # 11
+    Nop,       # 12
+    Store,     # 13
+    Fetch,     # 14
+    Jump,      # 15
+    JumpTrue,  # 16
+    JumpFalse, # 17
+    Call,      # 18
+    Arg,       # 19
+    Ret,       # 20
+    Print,     # 21
+    Halt,      # 22
 
-
-proc run(ops: seq[int]): int =
-  var ip: int
-  var op: OpCode
-  var stack: seq[int]
-  var memory: array[10, int]
+proc run(ops: seq[int], entry: int = 0, debug: bool = false): int =
+  var
+    ip: int = entry
+    fp: int
+    op: OpCode
+    stack: seq[int]
+    memory: array[0, int]
 
   while ip < ops.len:
     op = OpCode(ops[ip])
     inc(ip)
 
     case op
-    of True:
-      stack.add 1
-
-    of False:
+    of False: # 0
       stack.add 0
 
-    of Add:
-      var a = stack.pop
-      inc(ip)
-      var b = stack.pop
-      inc(ip)
+    of True: # 1
+      stack.add 1
+
+    of Add: # 2
+      var
+        b = stack.pop
+        a = stack.pop
       stack.add a + b
 
-    of Sub:
-      var a = stack.pop
-      inc(ip)
-      var b = stack.pop
-      inc(ip)
-      stack.add b - a
+    of Sub: # 3
+      var
+        b = stack.pop
+        a = stack.pop
+      stack.add a - b
 
-    of Mul:
-      var a = stack.pop
-      inc(ip)
-      var b = stack.pop
-      inc(ip)
+    of Mul: # 4
+      var
+        b = stack.pop
+        a = stack.pop
       stack.add a * b
 
-    of Div:
-      var a = stack.pop
-      inc(ip)
-      var b = stack.pop
-      inc(ip)
-      var q = a div b # quoient
-      var r = a mod b # remainder
-      stack.add r
+    of Div: # 5
+      var
+        b = stack.pop
+        a = stack.pop
+        q = a div b # quoient
       stack.add q
+      # var r = a mod b # remainder
+      # stack.add r
 
-    of Gt:
-      var a = stack.pop
-      inc(ip)
-      var b = stack.pop
-      inc(ip)
+    of Mod: # 6
+      var
+        b = stack.pop
+        a = stack.pop
+        r = a mod b # remainder
+      stack.add r
+
+    of Gt: # 7
+      var
+        b = stack.pop
+        a = stack.pop
       if a > b:
         stack.add ord(True)
       else:
         stack.add ord(False)
 
-    of Lt:
-      var a = stack.pop
-      inc(ip)
-      var b = stack.pop
-      inc(ip)
+    of Lt: # 8
+      var
+        b = stack.pop
+        a = stack.pop
       if a < b:
         stack.add ord(True)
       else:
         stack.add ord(False)
 
-    of Eq:
-      var a = stack.pop
-      inc(ip)
-      var b = stack.pop
-      inc(ip)
+    of Eq: # 9
+      var
+        b = stack.pop
+        a = stack.pop
       if a == b:
         stack.add ord(True)
       else:
         stack.add ord(False)
 
-    of Push:
+    of Push: # 10
       stack.add ops[ip]
       inc(ip)
 
-    of Pop:
+    of Pop: # 11
       discard stack.pop
+
+    of Nop: # 12
       inc(ip)
 
-    of Store:
+    of Store: # 13
       memory[stack.pop] = stack.pop
-      inc(ip)
-      inc(ip)
 
-    of Fetch:
+    of Fetch: # 14
       stack.add memory[stack.pop]
-      inc(ip)
 
-    of Jump:
-      inc(ip)
+    of Jump: # 15
       ip = ops[ip]
 
-    of JumpTrue:
-      inc(ip)
-      if ops[ip] == ord(True):
-        inc(ip)
+    of JumpTrue: # 16
+      if stack.pop == ord(True):
         ip = ops[ip]
-      inc(ip)
-
-    of JumpFalse:
-      inc(ip)
-      if ops[ip] == ord(False):
+      else:
         inc(ip)
+
+    of JumpFalse: # 17
+      if stack.pop == ord(False):
         ip = ops[ip]
+      else:
+        inc(ip)
+
+    of Call: # 18
+      var address = ops[ip]
       inc(ip)
 
+      stack.add ops[ip] # argc
+      inc(ip)
 
+      stack.add fp
+      stack.add ip
 
+      fp = stack.len - 1
+      ip = address
+
+    of Arg: # 19
+      stack.add stack[(fp - 3) - ops[ip]]
+      inc(ip)
+
+    of Ret: # 20
+      var returned = stack.pop
+      ip = stack.pop
+      fp = stack.pop
+
+      # pop args
+      for _ in countup(1, stack.pop):
+        discard stack.pop
+
+      stack.add returned
+
+    of Print: # 21
+      echo stack[stack.len - 1]
+
+    of Halt: # 22
+      break
+
+    if debug:
+      echo "---------------------"
+      echo " ip = ", ip, " | fp = ", fp
+      echo "    Code | ", $op, " (", $(ord(op) - 1), ")"
+      echo "   Stack | ", $stack
+      echo "  Memory | ", $memory
+      echo ""
 
   result = stack.pop
 
-echo @[ 9
-      , 10
-      , 9
-      , 20
-      , 2
-      ].run()
+
+# main
+
+# proc fact(n: int): int =
+#   return if n < 2: 1
+#              else: n * fact(n - 1)
+
+echo @[
+      # if n < 2: 1 else:
+        ord(Arg)       # 0
+      , 0              # 1
+      , ord(Push)      # 2
+      , 2              # 3
+      , ord(Lt)        # 4
+      , ord(JumpFalse) # 5
+      , 10             # 6
+      , ord(Push)      # 7
+      , 1              # 8
+      , ord(Ret)       # 9
+
+      # n * fact(n - 1)
+      , ord(Arg)       # 10
+      , 0              # 11
+      , ord(Arg)       # 12
+      , 0              # 13
+      , ord(Push)      # 14
+      , 1              # 15
+      , ord(Sub)       # 16
+      , ord(Call)      # 17
+      , 0              # 18
+      , 1              # 19
+      , ord(Mul)       # 20
+      , ord(Ret)       # 21
+
+      # proc fact(n: int): int
+      , ord(Push)      # 22 - entry
+      , 2              # 23 - input value
+      , ord(Call)      # 24
+      , 0              # 25
+      , 1              # 26
+      ].run( entry = 22
+           , debug = true)
